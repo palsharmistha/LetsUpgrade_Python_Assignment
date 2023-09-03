2262d2b1636e6d22bc68b09de3c3201c4f906399
palsharmistha <82878818+palsharmistha@users.noreply.github.com>
2023-09-03 12:58:11 +0530

    Add files via upload

---
+++ Assign1.py
@@ -0,0 +1,21 @@
+# Taking inputs from the user
+a = float(input("Enter the value of 'a': "))
+b = float(input("Enter the value of 'b': "))
+
+# Floor Division
+floor_division_result = a // b
+
+# Exponentiation (a power b)
+exponentiation_result = a ** b
+
+# Check if 'a' and 'b' have equal values
+equal_values = (a == b)
+
+# Display the results
+print("Floor Division Result:", floor_division_result)
+print("Exponentiation Result:", exponentiation_result)
+print("Are 'a' and 'b' Equal?", equal_values)
+
+
+
+
